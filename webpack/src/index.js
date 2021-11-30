218c1781c00e0c300e9c7487421d1f988b074b48
rdnrn <rodina.arina00@gmail.com>
2021-11-30 18:23:53 +0300

    Add style and html markup please work

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,14 +1,76 @@
-import _ from 'lodash';
+// eslint-disable-next-line max-classes-per-file
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+// To do class
+class ToDoList {
+  constructor(description, completed, id) {
+    this.description = description;
+    this.completed = completed;
+    this.id = id;
+  }
 }
 
-document.body.appendChild(component());
+// UI Class: handle UI Tasks
+
+class UI {
+  // display
+  static display() {
+    const toDoObj = [
+      {
+        description: 'adopt a cat',
+        completed: false,
+        id: Math.random().toString(16).slice(2),
+      },
+      {
+        description: 'bake a cake',
+        completed: false,
+        id: Math.random().toString(16).slice(2),
+      },
+    ];
+
+    const toDos = toDoObj;
+
+    toDos.forEach((toDo) => {
+      UI.addToDoToList(toDo);
+    });
+  }
+
+  // add todo to list
+
+  static addToDoToList(toDo) {
+    const toDoContainer = document.querySelector('.to-do-list');
+
+    const toDoItem = document.createElement('li');
+
+    toDoItem.innerHTML = `<div class="to-do-item">
+    <input type="checkbox" name="to-do" id="to-do">
+    <label for="to-do">${toDo.description}</label>
+    <div class="more">
+      <div class="more-dot"></div>
+      <div class="more-dot"></div>
+      <div class="more-dot"></div>
+    </div>
+    </div>`;
+
+    toDoContainer.appendChild(toDoItem);
+  }
+
+  // check todo as done
+
+  // remove todo ONE
+
+  // remove checked todos
+}
+
+// Event: Display ToDos
+
+document.addEventListener('DOMContentLoaded', UI.display);
+
+// Event: Add ToDos
+
+// Store Class: local storage
+
+// ADDIIONAL drag-and-drop items
+
+// INTERACTIVE LIST
+// ADD AND REMOVE ITEMS
